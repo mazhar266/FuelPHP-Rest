89b4b8c65c4c0bced0bcc5aca9e9ba88466e9468
Mazhar Ahmed <mazhar@seazoobd.com>
2019-05-10 12:29:49 +0600

    fixed the db config

--- fuel/app/config/db.php
+++ fuel/app/config/db.php
@@ -6,5 +6,13 @@
  */
 
 return array(
-
+    'default' => array(
+		'type'        => 'pdo',
+		'connection'  => array(
+			'dsn'      => 'sqlite:/sqlite.db',
+			'username' => '',
+			'password' => '',
+		),
+		'charset'  => NULL,
+    ),
 );
